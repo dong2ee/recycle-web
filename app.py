f3d678e21bb7e4b497e11f893307ac0ae5352986
Dong <81412557+dong2ee@users.noreply.github.com>
2022-12-28 20:19:59 +0900

    Update app.py
    select button

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ app = Flask(__name__)
 RESULT_FOLDER = os.path.join('static')
 app.config['RESULT_FOLDER'] = RESULT_FOLDER
 
+def get_model_names():
+    
+    model_names = []
+    for f  in os.listdir():
+        if f.endswith(".pt"):
+            model_names.append(f.split(".")[0])
+    return model_names
+
+
 
 def find_model():
     for f  in os.listdir():
@@ -19,14 +28,16 @@ def find_model():
             return f
     print("please place a model file in this directory!")
     
-model_name = find_model()
-model =torch.hub.load("WongKinYiu/yolov7", 'custom',model_name)
+global_model_name = find_model()
+model =torch.hub.load("WongKinYiu/yolov7", 'custom',global_model_name)
 
 model.eval()
 
-def get_prediction(img_bytes):
+def get_prediction(img_bytes, model_name):
     img = Image.open(io.BytesIO(img_bytes))
-    imgs = [img]  
+    imgs = [img]
+    model =torch.hub.load("WongKinYiu/yolov7", 'custom', model_name+".pt")
+    model.eval()
     results = model(imgs, size=640)  
     return results
 
@@ -36,17 +47,17 @@ def predict():
         if 'file' not in request.files:
             return redirect(request.url)
         file = request.files.get('file')
+        model_name = request.form["model_name"]
         if not file:
             return
             
         img_bytes = file.read()
-        results = get_prediction(img_bytes)
+        results = get_prediction(img_bytes, model_name=model_name)
         results.save(save_dir='static/result')
         filename = 'image0.jpg'
         
         return render_template('result.html',result_image = filename,model_name = model_name)
-
-    return render_template('index.html')
+    return render_template('index.html', model_names = get_model_names())
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
